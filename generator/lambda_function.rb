6070b31f2b56543a0b1ca5ed1170973d3e6b61ce
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-03-22 13:21:53 -0400

    Moving out poem of the day code and running it in a thread, as there will be other updates as well

--- generator/lambda_function.rb
+++ generator/lambda_function.rb
@@ -1,52 +1,16 @@
 require 'json'
 require 'aws-sdk-dynamodb'
 
+require_relative 'update_poem_of_the_day'
+
 def lambda_handler(event:, context:)
     dynamo_resource = Aws::DynamoDB::Resource.new({ region: 'us-east-2' })
-    table = dynamo_resource.table('Poem')
 
-    poemIds = table.scan({
-        projection_expression: 'poem_id',
-        expression_attribute_names: {
-            '#pid': 'poem_id'
-        },
-        expression_attribute_values: {
-            ':poemOfTheDay':  'poem-of-the-day'
-        },
-        filter_expression: '#pid <> :poemOfTheDay'
-    })
+    threads = []
 
-    poemIndex = rand(poemIds['count'])
+    threads += [ Thread.new { update_poem_of_the_day dynamo_resource } ]
 
-    poemId = poemIds['items'][poemIndex]['poem_id']
-
-    poemResponse = table.get_item({
-        table_name: 'Poem',
-        key: {
-            poem_id: poemId
-        }
-    })
-    
-    poem = poemResponse['item']
-
-    table.update_item({
-        key: {
-            poem_id: 'poem-of-the-day'
-        },
-        expression_attribute_names: {
-            '#Title': 'title',
-            '#Author': 'author',
-            '#Translator': 'translator',
-            '#Lines': 'lines'
-        }, 
-        expression_attribute_values: {
-            ':title': poem['title'],
-            ':author': poem['author'],
-            ':translator': poem['translator'],
-            ':lines': poem['lines']
-        }, 
-        update_expression: 'SET #Title = :title, #Author = :author, #Translator = :translator, #Lines = :lines', 
-    })
+    threads.each { |thread| thread.join }
 
     { statusCode: 200, body: JSON.generate({}) }
 end
---
+++ generator/update_poem_of_the_day.rb
@@ -0,0 +1,46 @@
+def update_poem_of_the_day(dynamo_resource)
+    table = dynamo_resource.table('Poem')
+
+    poemIds = table.scan({
+        projection_expression: 'poem_id',
+        expression_attribute_names: {
+            '#pid': 'poem_id'
+        },
+        expression_attribute_values: {
+            ':poemOfTheDay':  'poem-of-the-day'
+        },
+        filter_expression: '#pid <> :poemOfTheDay'
+    })
+
+    poemIndex = rand(poemIds['count'])
+
+    poemId = poemIds['items'][poemIndex]['poem_id']
+
+    poemResponse = table.get_item({
+        table_name: 'Poem',
+        key: {
+            poem_id: poemId
+        }
+    })
+    
+    poem = poemResponse['item']
+
+    table.update_item({
+        key: {
+            poem_id: 'poem-of-the-day'
+        },
+        expression_attribute_names: {
+            '#Title': 'title',
+            '#Author': 'author',
+            '#Translator': 'translator',
+            '#Lines': 'lines'
+        }, 
+        expression_attribute_values: {
+            ':title': poem['title'],
+            ':author': poem['author'],
+            ':translator': poem['translator'],
+            ':lines': poem['lines']
+        }, 
+        update_expression: 'SET #Title = :title, #Author = :author, #Translator = :translator, #Lines = :lines', 
+    })
+end
